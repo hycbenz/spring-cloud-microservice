aaffaaec360a27855929667275b1f924670d932d
hycbenz <1697305693@qq.com>
2020-03-25 10:16:42 +0800

    EurekaServer集群配置

---
+++ microservicecloud-eureka-7002/src/main/java/com/atguigu/springcloud/EurekaServer7002_App.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 这里是类说明.
+ *
+ * @className: EurekaServer7002_App
+ * @author: 胡永持
+ * @date: 2020-03-24 15:32
+ */
+@EnableEurekaServer//表示是eureka的server 会接受其它微服务注册进来
+@SpringBootApplication
+public class EurekaServer7002_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7002_App.class, args);
+    }
+}
